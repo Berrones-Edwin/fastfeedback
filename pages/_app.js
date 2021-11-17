7d0fc46d65813bbc710736e3be6293c2d17a58e4
Berrones Edwin <Berrones.Edwin@hotmail.com>
2021-11-17 12:14:32 -0600

    [Feature] Add custom theme file

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import { ProvideAuth } from '@/lib/auth'
+import theme from '@/styles/theme'
 import { ChakraProvider } from '@chakra-ui/react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <ProvideAuth>
         <Component {...pageProps} />
       </ProvideAuth>
