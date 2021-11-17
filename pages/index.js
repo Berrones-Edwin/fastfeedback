1fe94848f88675a5efa60760ab0ea99ffb915e78
Berrones Edwin <Berrones.Edwin@hotmail.com>
2021-11-17 12:15:11 -0600

    [Refactor] refactor in index app

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,32 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { useAuth } from '../lib/auth'
-import styles from '../styles/Home.module.css'
+import { useAuth } from '@/lib/auth'
+import { Heading, Flex, Text, Button } from '@chakra-ui/react'
 
 export default function Home() {
   const auth = useAuth()
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Fast Feedback</title>
-        <meta name="description" content="App for description fast feedback in static app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <Flex
+        as="main"
+        direction="column"
+        justify="center"
+        align="center"
+        h="100vh"
+      >
+        <Head>
+          <title>Fast Feedback</title>
+        </Head>
 
-      <main className={styles.main}>
+        <Heading>Fast Feedback</Heading>
         {auth?.user ? (
           <>
-            <h3>Current user : {auth.user.name}</h3>
-            <button onClick={auth.signOut}>Sign out</button>
+            <Text>Current user : {auth.user.name}</Text>
+            <Button onClick={auth.signOut}>Sign out</Button>
           </>
         ) : (
-          <button onClick={auth.signInWithGithub}>Sign In with github</button>
+          <Button onClick={auth.signInWithGithub}>Sign In with github</Button>
         )}
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
+      </Flex>
+    </>
   )
 }
