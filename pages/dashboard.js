875660a47e8128270e0e5b995232d4a128dc4043
Berrones Edwin <Berrones.Edwin@hotmail.com>
2021-11-22 11:53:13 -0600

    [Refactor] Implements hook swr and add new components

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,12 +1,30 @@
 import Head from 'next'
 import EmptyState from '@/components/EmptyState'
 import { useAuth } from '@/lib/auth'
+import useSWR from 'swr'
+import fetcher from '@/utils/fetcher'
+import DashboardShell from '@/components/DashboardShell'
+import SiteTableSkeleton from '@/components/SiteTableSkeleton'
+import SiteTable from '@/components/SiteTable'
 
 const Dashboard = () => {
   const auth = useAuth()
+  const { data } = useSWR('/api/sites', fetcher)
+  console.log(data);
 
   if (!auth.user) return 'Loading....'
-  return <EmptyState />
+  if (!data) {
+    return (
+      <DashboardShell>
+        <SiteTableSkeleton />
+      </DashboardShell>
+    )
+  }
+  return (
+    <DashboardShell>
+      {data ? <SiteTable sites={data} /> : <EmptyState />}
+    </DashboardShell>
+  )
 }
 
 export default Dashboard
