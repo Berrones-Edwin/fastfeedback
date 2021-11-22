a73fb494eb1a1acb941dfa6a75736025ccb89b16
Berrones Edwin <Berrones.Edwin@hotmail.com>
2021-11-22 11:54:39 -0600

    [Refactor] Remove Dashboardshell and add modal site

--- components/EmptyState.js
+++ components/EmptyState.js
@@ -1,28 +1,25 @@
 import React from 'react'
 import { Flex, Heading, Text } from '@chakra-ui/react'
 
-import DashboardShell from './DashboardShell'
 import AddModalSite from './AddModalSite'
 
 const EmptyState = () => {
   return (
-    <DashboardShell>
-      <Flex
-        width="100%"
-        borderRadius="8px"
-        p={16}
-        justify="center"
-        align="center"
-        direction="column"
-        border={'1px solid #FFF'}
-      >
-        <Heading size="lg" mb={2}>
-          You haven’t added any sites.
-        </Heading>
-        <Text mb={4}>Let’s get started.</Text>
-        <AddModalSite />
-      </Flex>
-    </DashboardShell>
+    <Flex
+      width="100%"
+      borderRadius="8px"
+      p={16}
+      justify="center"
+      align="center"
+      direction="column"
+      border={'1px solid #FFF'}
+    >
+      <Heading size="lg" mb={2}>
+        You haven’t added any sites.
+      </Heading>
+      <Text mb={4}>Let’s get started.</Text>
+      <AddModalSite>Add your first site</AddModalSite>
+    </Flex>
   )
 }
 
