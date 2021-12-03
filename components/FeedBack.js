26c54734293546003299d2c38e3d3bbc507612e8
Berrones Edwin <Berrones.Edwin@hotmail.com>
2021-12-03 10:53:26 -0600

    [Feature] component show individual feedback

---
+++ components/FeedBack.js
@@ -0,0 +1,20 @@
+import { Box, Divider, Heading, Text } from '@chakra-ui/react'
+
+const FeedBack = ({ author, text, createdAT }) => {
+  return (
+    <Box borderRadius={4} maxW="700px" w="full">
+      <Heading fontWeight="medium" size="sm" as="h3" mb={0} color="white.900">
+        {author}
+      </Heading>
+      <Text color="white.500" mb={4} fontSize="xs">
+        {' '}
+        {/* {format(parseISO(createdAT), 'PPpp')}{' '} */}
+        { createdAT }
+      </Text>
+      <Text color="white.800">{text}</Text>
+      <Divider borderColor="white.200" bgColor="white.200" mt={8} mb={8} />
+    </Box>
+  )
+}
+
+export default FeedBack
