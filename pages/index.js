879d106deda3647e7fa4329b8157133912add5ed
Berrones Edwin <Berrones.Edwin@hotmail.com>
2021-12-04 14:01:56 -0600

    [Refactor] Delete script

--- pages/index.js
+++ pages/index.js
@@ -15,13 +15,25 @@ export default function Home() {
       >
         <Head>
           <title>Fast Feedback</title>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              if (document.cookie && document.cookie.includes('auth')) {
+                window.location.href = "/dashboard"
+              }
+            `
+            }}
+          />
         </Head>
 
         <Heading>Fast Feedback</Heading>
         {auth?.user ? (
           <>
             <Text>Current user : {auth.user.name}</Text>
-            <Button onClick={auth.signOut}>Sign out</Button>
+            <Button as="a" size="sm" fontWeight="medium" href="/dashboard">
+              View Dashboard
+            </Button>
+            {/* <Button onClick={auth.signOut}>Sign out</Button> */}
           </>
         ) : (
           <Button onClick={auth.signInWithGithub}>Sign In with github</Button>
