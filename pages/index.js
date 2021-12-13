1373aad53ab7302c3424cba7f765e3fe72001509
Berrones Edwin <Berrones.Edwin@hotmail.com>
2021-12-13 15:52:20 -0600

    [Feature] New button for login with google

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,106 @@
-import Head from 'next/head'
 import { useAuth } from '@/lib/auth'
-import { Heading, Flex, Text, Button } from '@chakra-ui/react'
-
+import { Box, Button, Flex, Link, Stack, Text } from '@chakra-ui/react'
+import Head from 'next/head'
 export default function Home() {
   const auth = useAuth()
   return (
     <>
-      <Flex
-        as="main"
-        direction="column"
-        justify="center"
-        align="center"
-        h="100vh"
-      >
-        <Head>
-          <title>Fast Feedback</title>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
+      <Box bg="gray.100">
+        <Flex
+          as="main"
+          direction="column"
+          justify="center"
+          align="center"
+          h="100vh"
+        >
+          <Head>
+            <title>Fast Feedback</title>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
               if (document.cookie && document.cookie.includes('auth')) {
                 window.location.href = "/dashboard"
               }
             `
-            }}
-          />
-        </Head>
+              }}
+            />
+          </Head>
 
-        <Heading>Fast Feedback</Heading>
-        {auth?.user ? (
-          <>
-            <Text>Current user : {auth.user.name}</Text>
-            <Button as="a" size="sm" fontWeight="medium" href="/dashboard">
-              View Dashboard
-            </Button>
-            {/* <Button onClick={auth.signOut}>Sign out</Button> */}
-          </>
-        ) : (
-          <Button onClick={auth.signInWithGithub}>Sign In with github</Button>
-        )}
-      </Flex>
+          <Text mb={4} fontSize="lg" p={6}>
+            <Text as="span" fontWeight="bold" display="inline">
+              Fast Feedback
+            </Text>
+            {' is being built as part of '}
+            <Link
+              href="https://react2025.com"
+              isExternal
+              textDecoration="underline"
+            >
+              Sign In with GitHub React 2025
+            </Link>
+            {`. It's the easiest way to add comments or reviews to your static site. It's still a work-in-progress, but you can try it out by logging in.`}
+          </Text>
+          {auth?.user ? (
+            <>
+              <Text>Current user : {auth.user.name}</Text>
+              <Button
+                as="a"
+                href="/dashboard"
+                backgroundColor="white"
+                color="gray.900"
+                variant="outline"
+                fontWeight="medium"
+                leftIcon="google"
+                mt={4}
+                size="lg"
+                _hover={{ bg: 'gray.100' }}
+                _active={{
+                  bg: 'gray.100',
+                  transform: 'scale(0.95)'
+                }}
+              >
+                View Dashboard
+              </Button>
+            </>
+          ) : (
+            <Stack>
+              <Button
+                onClick={e => auth.signInWithGithub()}
+                backgroundColor="gray.900"
+                color="white"
+                fontWeight="medium"
+                // leftIcon="github"
+                mt={4}
+                size="lg"
+                _hover={{ bg: 'gray.700' }}
+                _active={{
+                  bg: 'gray.800',
+                  transform: 'scale(0.95)'
+                }}
+              >
+                Sign In with GitHub
+              </Button>
+              <Button
+                onClick={e => auth.signInWithGoogle()}
+                backgroundColor="white"
+                color="gray.900"
+                variant="outline"
+                fontWeight="medium"
+                leftIcon="google"
+                mt={4}
+                size="lg"
+                _hover={{ bg: 'gray.100' }}
+                _active={{
+                  bg: 'gray.100',
+                  transform: 'scale(0.95)'
+                }}
+              >
+                Sign In with Google
+              </Button>
+            </Stack>
+          )}
+        </Flex>
+      </Box>
     </>
   )
 }
