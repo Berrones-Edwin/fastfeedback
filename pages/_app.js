fc87b3bffeba7ee9448259684430685055c0f96d
Berrones Edwin <Berrones.Edwin@hotmail.com>
2021-12-13 15:52:00 -0600

    [style] Add global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,34 @@
 import { ProvideAuth } from '@/lib/auth'
 import theme from '@/styles/theme'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, CSSReset } from '@chakra-ui/react'
+import { css, Global } from '@emotion/react'
 import '../styles/globals.css'
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  )
+}
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+      <GlobalStyle />
       <ProvideAuth>
         <Component {...pageProps} />
       </ProvideAuth>
